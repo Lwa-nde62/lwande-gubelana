555c5b198ef83f5ddacea1a6a2e68d426e38e1d6
Lwa-nde62 <122478177+Lwa-nde62@users.noreply.github.com>
2025-02-14 15:29:11 +0200

    Added calendar generator script

---
+++ calendar_generator.py
@@ -0,0 +1,10 @@
+import calendar
+
+def display_calendar(year):
+    cal = calendar.TextCalendar()
+    for month in range(1, 13):
+        print(cal.formatmonth(year, months))
+
+# Example usage
+year = 2025  # Change to desired year
+display_calendar(year)
